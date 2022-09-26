0c997a9a8d8fcc8999ab3d1c2c14ee082444aaee
Somov Andrii <som75off@gmail.com>
2022-09-26 23:49:55 +0200

    git init

---
+++ src/main/java/com/hillel/somov/lessons/lesson9/Matrix.java
@@ -0,0 +1,59 @@
+package com.hillel.somov.lessons.lesson9;
+
+import java.util.Scanner;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rows");
+        int m = sc.nextInt();
+
+        System.out.println("Enter columns");
+        int n = sc.nextInt();
+
+        if (m < 0 || n < 0) {
+            System.out.println("Value cannot be negative");
+            System.exit(0);
+        }
+        System.out.println("Enter max numbers");
+        int max = sc.nextInt();
+
+        int[][] array1 = new int[m][];
+        int[][] array2 = new int[n][];
+
+        for (int i = 0; i < array2.length; i++)
+            array2[i] = new int[m];
+
+        System.out.println("BEFORE");
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = new int[n];
+            for (int j = 0; j < array1[i].length; j++) {
+                array1[i][j] = ((int) (Math.random() * max));
+                System.out.print(array1[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = array1[j][i];
+
+            }
+        }
+
+        System.out.println("AFTER");
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
