64a0fca341a0259beeb8a4bab3b8d5caff619206
Somov Andrii <som75off@gmail.com>
2022-11-11 19:29:37 +0100

    git init

--- src/main/java/com/hillel/somov/javaPRO/lessons/lesson2/Employee.java
+++ src/main/java/com/hillel/somov/javaPRO/lessons/lesson2/Employee.java
@@ -5,14 +5,16 @@ public class Employee {
     private String surname;
     private int age;
     private String position;
+    private String email;
     private int salary;
 
-    public Employee(String name, String surname, int age, String position, int salary) {
+    public Employee(String name, String surname, int age, String position, int salary, String email) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.position = position;
         this.salary = salary;
+        this.email = email;
     }
 
     public String getName() {
@@ -33,6 +35,9 @@ public class Employee {
 
     public int getSalary() {
         return salary;
+    }
+    public String getEmail() {
+        return email;
     }
 
     public void setName(String name) {
@@ -55,8 +60,12 @@ public class Employee {
         this.salary = salary;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void printEmployee() {
-        System.out.println("Name: " + name + " Surname: " + surname + " Age: " + age + " Position: " + position + " Salary: " + salary);
+        System.out.println("Name: " + name + " Surname: " + surname + " Age: " + age + " Position: " + position + " Salary: " + salary + " Email: " + email);
     }
 }
 
--- src/main/java/com/hillel/somov/javaPRO/lessons/lesson2/Main.java
+++ src/main/java/com/hillel/somov/javaPRO/lessons/lesson2/Main.java
@@ -3,7 +3,7 @@ package com.hillel.somov.javaPRO.lessons.lesson2;
 public class Main {
     public static void main(String[] args) {
 
-        Employee employee = new Employee("Bob", "Marley", 33, "Musician", 2000);
+        Employee employee = new Employee("Bob", "Marley", 33, "Musician", 2000,"bob@gmail.com");
         employee.printEmployee();
 
         Car car = new Car();
